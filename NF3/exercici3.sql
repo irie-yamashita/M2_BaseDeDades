77f7b1e3a232ddd17ea2f8a69918a4c4fa16410d
irieyamashita <irieyamashita3d@gmail.com>
2025-03-12 15:58:44 +0100

    Update exercici3.sql

--- NF3/exercici3.sql
+++ NF3/exercici3.sql
@@ -1,5 +1,9 @@
+/*EA3. Estructures de control de flux*/
 
-/*1)*/
+
+/*1)Exercici 1. Escriu un programa PL/SQL que introduirem per teclat dos números.
+Els dos números han de ser positius, en cas contrari s’ha de mostrar a l’usuari el missatge  corresponent.
+S’ha de realitzar la següent operació amb aquests números: dividir entre ells i sumar-li el segon i mostrar el resultat de l'operació.*/
 DO
 $$
     DECLARE
@@ -17,7 +21,9 @@ $$
 $$ language plpgsql;
 
 
-/*2)*/
+
+/*2) Exercici 2. Escriu el mateix programa PL/SQL de l’exercici 1, però ara també s’ha de controlar que el primer número sigui més gran que el segon.
+En cas contrari s’ha de mostrar el següent missatge: ‘Error! el primer número ha de ser més gran que el segon’.*/
 DO
 $$
     DECLARE
@@ -37,7 +43,12 @@ $$
 $$ language plpgsql;
 
 
-/*3)*/
+
+/*3)Exercici 3. Escriu un programa PL/SQL que demani a l’usuari la seva edat i mostri el missatge corresponent, si:
+a) Entre 0 i 17 mostres 'Ets menor de edat!'
+b) Entre 18 i 40 mostres 'Ja ets major de edat!'
+d) > 40 mostres 'ja ets força gran'
+e) Si és negatiu (<0) mostres 'L ́edat no pot ser negativa'.*/
 DO
 $$
     DECLARE
@@ -78,7 +89,9 @@ $$
 $$ language plpgsql;
 
 
-/*4)*/
+/*4)Exercici 4. Escriu un programa PL/SQL que demani quina operació es farà:
+opció 1 SUMAR, opció 2 RESTAR, opció 3 MULTIPLICAR, opció 4 DIVIDIR .
+Després el programa també demana dos números i ha de realitzar la operació escollida amb els dos números introduits per teclat. S’ha de mostrar l’operació escollida, els números  introduïts i el resultat.*/
 DO
 $$
     DECLARE
@@ -106,7 +119,7 @@ $$
 $$ language plpgsql;
 
 
-/*5)*/
+/*5)Exercici 5. Escriu un programa PL/SQL que ens mostri els números entre un rang. El rang mínim és 1 i el màxim se li ha de preguntar a l’usuari i no pot ser menor que 2. Si no és 2 o més gran es mostra un missatge a l'usuari i finalitza el programa. Resol l’exercici utilitzant l’estructura FOR i després l’estructura WHILE.*/
 
     --for
 DO
@@ -146,6 +159,26 @@ $$
 $$ language plpgsql;
 
 
-/*6)*/
-
+/*6)Exercici 6. Escriu un programa PL/SQL que mostri els números entre un rang amb un salt. Tant el rang mínim, com el màxim i el salt se li ha de preguntar a l’usuari. A més, s’ha de tenir en compte que el rang mínim sempre ha de ser més petit que el rang màxim i que el el salt ha de ser més gran que 1. En cas contrari s’ha de mostrar el missatge corresponent i acabar el programa.Utilitza l'estructura de control de flux LOOP.*/
+DO
+$$
+    DECLARE
+        min NUMERIC = :v_min;
+        max NUMERIC = :v_max;
+        salt NUMERIC = :v_salt;
+        i NUMERIC;
+    BEGIN
+        i= min;
+        IF min > max THEN
+            raise notice 'El mínim no pot ser superior al màxim!';
+        ELSEIF salt <= 1 THEN
+            raise notice 'El salt ha de ser superior a 1!';
+        ELSE
+            WHILE i <= max LOOP
+                raise notice '% ', i;
+                i = i+salt;
+            END LOOP;
+        END IF;
+    END;
+$$ language plpgsql;
 
